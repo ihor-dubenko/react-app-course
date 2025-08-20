19fe36525775e160bb11a3e903edc72251bfc233
Ihor Dubenko <dubychne87@gmail.com>
2025-08-21 02:23:09 +0300

    main layout routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,21 @@
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
-import "./App.css";
-import { Counter } from "./Counter.jsx";
-import { List } from "./List.jsx";
+import { MainLayout } from "./components/MainLayout";
+import {
+  BrowserRouter,
+  Route,
+  Routes
+} from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-          <Counter/>
-          <List/>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-    </>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<div>home</div>} />
+            <Route path="/forbidden" element={<div>forbidden</div>} />
+            <Route path="/addquestion" element={<div>add question</div>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
   )
 }
 
---
+++ src/components/MainLayout/MainLayout.jsx
@@ -0,0 +1,20 @@
+import cls from "./MainLayout.module.css";
+import { Outlet } from "react-router-dom";
+
+export const MainLayout = () => {
+  const currentYear = new Date().getFullYear();
+  return (
+    <div className={cls.mainLayout}>
+      <header>header</header>
+      <div className={cls.mainWrapper}>
+        <main className={cls.main}>
+          <Outlet />
+        </main>
+        <footer className={cls.footer}>
+          React Question Cards Application | {currentYear} <br/>
+          by Ihor Dubenko
+        </footer>
+      </div>
+    </div>
+  );
+}
