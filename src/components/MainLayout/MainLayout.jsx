574633aa50a74d58e830c55c39dc6fda1c000ba9
Ihor Dubenko <dubychne87@gmail.com>
2025-08-21 14:06:49 +0300

    updated header

--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -1,11 +1,12 @@
 import cls from "./MainLayout.module.css";
 import { Outlet } from "react-router-dom";
+import { Header } from "../header";
 
 export const MainLayout = () => {
   const currentYear = new Date().getFullYear();
   return (
     <div className={cls.mainLayout}>
-      <header>header</header>
+      <Header />
       <div className={cls.mainWrapper}>
         <main className={cls.main}>
           <Outlet />
