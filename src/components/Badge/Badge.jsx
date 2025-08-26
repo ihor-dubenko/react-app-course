776aba76a1f79789bb9ea6a930280504c0ebf1b0
Ihor Dubenko <dubychne87@gmail.com>
2025-08-26 22:30:52 +0300

    updated

---
+++ src/components/Badge/Badge.jsx
@@ -0,0 +1,14 @@
+import cls from "./Badge.module.css"
+
+export const Badge = ({ variant, children }) => {
+  switch (variant) {
+    case "primary":
+      return <div className={`${cls.badge} ${cls.primary}`}> { children } </div>;
+    case "warning":
+      return <div className={`${cls.badge} ${cls.warning}`}> { children } </div>;
+    case "alert":
+      return <div className={`${cls.badge} ${cls.alert}`}> { children } </div>;
+    default:
+      return <div className={`${cls.badge} ${cls.success}`}> { children } </div>;
+  }
+}
--- src/components/QuestionCard/QuestionCard.jsx
+++ src/components/QuestionCard/QuestionCard.jsx
@@ -1,29 +1,32 @@
 import cls from "./QuestionCard.module.css";
 import { Button } from "../Button/index.jsx";
 import { useNavigate } from "react-router-dom";
+import { Badge } from "../Badge/index.jsx";
 
 export const QuestionCard = ({ card }) => {
   const navigate = useNavigate();
+  const levelVariant = card.level === 1 ? "primary" : card.level === 2 ? "warning" : "alert";
+  const completedVariant = card.completed ? "success" : "primary";
   return (
-      <div className={cls.card}>
-        <div className={cls.cardLabels}>
-          <div>Level: {card.level}</div>
-          <div>{card.completed ? "Completed" : "Not Completed"}</div>
-        </div>
-        <h5 className={cls.cardTitle}>
-          {card.question}
-        </h5>
-        <div className={cls.cardAnswers}>
-          <label>Short answer:</label>
-          <p className={cls.cardAnswer}>
-            {card.answer}
-          </p>
-        </div>
-        <Button
-            onClick={() => navigate(`/question/${card.id}`)}
-        >
-          View
-        </Button>
+    <div className={cls.card}>
+      <div className={cls.cardLabels}>
+        <Badge variant={levelVariant}>Level: {card.level}</Badge>
+        <Badge variant={completedVariant}>{card.completed ? "Completed" : "Not Completed"}</Badge>
       </div>
+      <h5 className={cls.cardTitle}>
+        {card.question}
+      </h5>
+      <div className={cls.cardAnswers}>
+        <label>Short answer:</label>
+        <p className={cls.cardAnswer}>
+          {card.answer}
+        </p>
+      </div>
+      <Button
+          onClick={() => navigate(`/question/${card.id}`)}
+      >
+        View
+      </Button>
+    </div>
   )
 }
