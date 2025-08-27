c8be3a7c43f86edb596ca683053d95d8b0ef9d89
Ihor Dubenko <dubychne87@gmail.com>
2025-08-27 16:28:29 +0300

    updated

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -22,11 +22,21 @@ const sortOptions = [
   { value: "_sort=-completed", label: "Completed DESC" }
 ];
 
+const perPageOptions = [
+  { value: "10", label: "10" },
+  { value: "20", label: "20" },
+  { value: "25", label: "25" },
+  { value: "50", label: "50" },
+  { value: "100", label: "100" },
+];
+
 export const HomePage = () => {
   const [searchParams, setSearchParams] = useState(`?_page=1&_per_page=${DEFAULT_PER_PAGE}`);
   const [questions, setQuestions] = useState({});
   const [searchValue, setSearchValue] = useState("");
   const [sortSelectValue, setSortSelectValue] = useState("");
+  const [perPageSelectValue, setPerPageSelectValue] = useState(perPageOptions[0].value);
+
   const [getQuestions, isLoading, error] = useFetch(async (url) => {
     const response = await fetch(`${API_URL}/${url}`);
     const questions = await response.json();
@@ -64,12 +74,16 @@ export const HomePage = () => {
 
   const onSortSelectChangeHandler = (e) => {
     setSortSelectValue(e.target.value);
+    setSearchParams(`?_page=1&_per_page=${perPageSelectValue}&${e.target.value}`);
+  }
 
-    setSearchParams(`?_page=1&_per_page=${DEFAULT_PER_PAGE}&${e.target.value}`);
+  const onPerPageSelectChangeHandler = (e) => {
+    setPerPageSelectValue(e.target.value);
+    setSearchParams(`?_page=1&_per_page=${e.target.value}&${sortSelectValue}`);
   }
 
   const onPageChangeHandler = (page) => {
-    setSearchParams(`?_page=${page}&_per_page=${DEFAULT_PER_PAGE}&${sortSelectValue}`);
+    setSearchParams(`?_page=${page}&_per_page=${perPageSelectValue}&${sortSelectValue}`);
     controlContainerRef.current.scrollIntoView({ behavior: "smooth" });
   }
 
@@ -85,6 +99,11 @@ export const HomePage = () => {
           onChange={onSortSelectChangeHandler}
           options={sortOptions}
         />
+        <Select
+            value={perPageSelectValue}
+            onChange={onPerPageSelectChangeHandler}
+            options={perPageOptions}
+        />
       </div>
       { isLoading && <Loader /> }
       { error && <p>{error}</p> }
@@ -93,6 +112,7 @@ export const HomePage = () => {
         (
           <p className={cls.noCardsInfo}>No cards...</p>
         ) :
+        pagination.length > 1 &&
         (
           <div className={cls.paginationContainer}>
             {
